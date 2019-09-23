4131f58ae9568ab52b76f36384668615e789a17a
mkarem4 <m.karem456@gmail.com>
2019-09-23 23:15:45 +0200

    add new dashboard

--- public/admin/plugins/datatables/basic/customdt.js
+++
@@ -1,1 +0,0 @@
-var DatatablesBasicBasic={init:function(){var e;(e=$("#m_table_1")).DataTable({responsive:!0,dom:"<'row'<'col-sm-12'tr>>\n\t\t\t<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>",lengthMenu:[-1,10,25,50],pageLength:10,language:{lengthMenu:"Display _MENU_"},order:[[1,"desc"]]})}};jQuery(document).ready(function(){DatatablesBasicBasic.init()});
