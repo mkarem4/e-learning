bca402ca6dbb5625721aebcbdaf47eb9286e0a33
mkarem4 <m.karem456@gmail.com>
2019-09-27 13:55:30 +0200

    display courses in courses page

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -9,7 +9,12 @@ class CourseController extends Controller
 {
     public function index()
     {
-        $courses = Course::all();
+        if (auth()->user()->type == 2)
+            $courses = Course::where('user_id', auth()->user()->id)->get();
+        elseif (auth()->user()->type == 3)
+            $courses = Course::where('level_id', auth()->user()->level)->get();
+        else
+            $courses = Course::all();
         return view('courses.index', compact('courses'));
     }
 
